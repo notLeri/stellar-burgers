f324138ea697413704e20583475ad357b6dc0e62
notLeri <ed.nazarov001@gmail.com>
2024-12-01 16:13:06 +0300

    feat: added last feeds for customer

--- src/components/order-card/order-card.tsx
+++ src/components/order-card/order-card.tsx
@@ -1,17 +1,22 @@
-import { FC, memo, useMemo } from 'react';
+import { FC, memo, useEffect, useMemo } from 'react';
 import { useLocation } from 'react-router-dom';
 
 import { OrderCardProps } from './type';
 import { TIngredient } from '@utils-types';
 import { OrderCardUI } from '../ui/order-card';
+import { useDispatch, useSelector } from '../../services/store';
+import { getIngredients, ingredientsSelector } from '@slices';
 
 const maxIngredients = 6;
 
 export const OrderCard: FC<OrderCardProps> = memo(({ order }) => {
   const location = useLocation();
+  const dispatch = useDispatch();
+  const ingredients = useSelector(ingredientsSelector);
 
-  /** TODO: взять переменную из стора */
-  const ingredients: TIngredient[] = [];
+  useEffect(() => {
+    dispatch(getIngredients());
+  }, []);
 
   const orderInfo = useMemo(() => {
     if (!ingredients.length) return null;
--- src/pages/feed/feed.tsx
+++ src/pages/feed/feed.tsx
@@ -1,15 +1,24 @@
 import { Preloader } from '@ui';
 import { FeedUI } from '@ui-pages';
-import { TOrder } from '@utils-types';
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
+import { useDispatch, useSelector } from '../../services/store';
+import { getFeeds, ordersSelector } from '@slices';
 
 export const Feed: FC = () => {
-  /** TODO: взять переменную из стора */
-  const orders: TOrder[] = [];
+  const dispatch = useDispatch();
+  const orders = useSelector(ordersSelector);
+
+  useEffect(() => {
+    dispatch(getFeeds());
+  }, []);
+
+  function handleGetFeeds() {
+    dispatch(getFeeds());
+  }
 
   if (!orders.length) {
     return <Preloader />;
   }
 
-  <FeedUI orders={orders} handleGetFeeds={() => {}} />;
+  return <FeedUI orders={orders} handleGetFeeds={handleGetFeeds} />;
 };
